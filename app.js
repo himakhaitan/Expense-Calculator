a5b86bc8d0271667b4198d3d7da1f79786b39c08
hima_khaitan <himanshukhaitan108@gmail.com>
2021-03-09 09:54:56 +0530

    Input Data Tapped

--- app.js
+++ app.js
@@ -2,20 +2,52 @@
 var budgetController = (function () {})();
 
 // UI CONTROLLER
-var UIcontroller = (function () {})();
+var UIcontroller = (function () {
+
+  var DOMstrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn'
+  }
+
+  return {
+    getInput: function()  {
+      var type = document.querySelector(DOMstrings.inputType).value; // Will be either inc or exp.
+      var description = document.querySelector(DOMstrings.inputDescription).value;
+      var value = document.querySelector(DOMstrings.inputValue).value;
+
+      return {
+        type,
+        description,
+        value
+      }
+    },
+    getDOMstrings: function() {
+      return DOMstrings;
+    }
+  };
+
+
+
+})();
 
 // GLOBAL APP CONTROL
 var controller = (function (budgetCtrl, UICtrl) {
+
+  var DOM = UICtrl.getDOMstrings();
   var ctrlAddItem = function () {
     // Get Input Data
+
+    var input = UICtrl.getInput();
+    console.log(input)
     // Add item to the budget controller
     // Add the new item to user interface
     // calculate the budget
     // Display the budget in the UI
-    console.log('test passed!')
   };
 
-  document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
+  document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
 
   document.addEventListener("keypress", function (event) {
     if (event.keyCode === 13 || event.which === 13) {
